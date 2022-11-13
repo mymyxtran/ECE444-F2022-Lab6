947cf750e6fc72060ea76671b29bad83293086b7
mymyxtran <57696924+mymyxtran@users.noreply.github.com>
2022-11-12 21:16:25 -0500

    POSTGRES HEROKU, Login Required, ..ETC

--- project/app.py
+++ project/app.py
@@ -1,8 +1,11 @@
 import sqlite3
 from pathlib import Path
-
+import os
 from flask import Flask, g, render_template, request, session, \
                   flash, redirect, url_for, abort, jsonify
+
+from functools import wraps
+
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -13,9 +16,14 @@ DATABASE = "flaskr.db"
 USERNAME = "admin"
 PASSWORD = "admin"
 SECRET_KEY = "change_me"
-SQLALCHEMY_DATABASE_URI = f'sqlite:///{Path(basedir).joinpath(DATABASE)}'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+url = os.getenv('DATABASE_URL', f'sqlite:///{Path(basedir).joinpath(DATABASE)}')
+
+if url.startswith("postgres://"):
+    url = url.replace("postgres://", "postgresql://", 1)
+
+SQLALCHEMY_DATABASE_URI = url
 
 # create and initialize a new Flask app
 app = Flask(__name__)
@@ -61,6 +69,13 @@ def login():
             return redirect(url_for('index'))
     return render_template('login.html', error=error)
 
+@app.route('/search/', methods=['GET'])
+def search():
+    query = request.args.get("query")
+    entries = db.session.query(models.Post)
+    if query:
+        return render_template('search.html', entries=entries, query=query)
+    return render_template('search.html')
 
 @app.route('/logout')
 def logout():
@@ -69,13 +84,23 @@ def logout():
     flash('You were logged out')
     return redirect(url_for('index'))
 
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not session.get('logged_in'):
+            flash('Please log in.')
+            return jsonify({'status': 0, 'message': 'Please log in.'}), 401
+        return f(*args, **kwargs)
+    return decorated_function
 
 @app.route('/delete/<int:post_id>', methods=['GET'])
+@login_required
 def delete_entry(post_id):
     """Deletes post from database."""
     result = {'status': 0, 'message': 'Error'}
     try:
-        db.session.query(models.Post).filter_by(id=post_id).delete()
+        new_id = post_id
+        db.session.query(models.Post).filter_by(id=new_id).delete()
         db.session.commit()
         result = {'status': 1, 'message': "Post Deleted"}
         flash('The entry was deleted.')
